38299f0a785878cee987ba6582d42987ed9d0f48
Erik <sertvitas@gmail.com>
2023-11-20 09:17:56 -0500

    simple shit

--- cmd/db_check/main.go
+++ cmd/db_check/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 
+	"github.com/sertvitas/db_check/deleteme"
 	"github.com/sertvitas/db_check/version"
 
 	"github.com/rs/zerolog"
@@ -13,4 +14,6 @@ func main() {
 	logger := zerolog.New(os.Stderr).With().Str("version", version.Version).Timestamp().Logger()
 	logger.Info().Msg("starting some other shit")
 
+	result := deleteme.Add(1, 2)
+	logger.Info().Msgf("result %d", result)
 }
---
+++ deleteme/deleteme.go
@@ -0,0 +1,6 @@
+package deleteme
+
+// Add sum two integers
+func Add(a, b int) int {
+	return a + b
+}
