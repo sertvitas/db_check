bc535cbe76fdc3cb4d7f9b63a271003efd968585
Erik <sertvitas@gmail.com>
2023-11-20 13:02:26 -0500

    updating things

--- cmd/db_check/main.go
+++ cmd/db_check/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/sertvitas/db_check/deleteme"
 	"github.com/sertvitas/db_check/version"
@@ -9,6 +10,19 @@ import (
 	"github.com/rs/zerolog"
 )
 
+func getcreds(db string) string {
+	return "creds for " + db + " from vault"
+}
+
+func checktcp(port int) string {
+	return "checking tcp port " + strconv.Itoa(port)
+}
+
+func dblogin(auth string) string {
+	return "logging into CTS db with " + auth
+}
+
+// func dbping()
 func main() {
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	logger := zerolog.New(os.Stderr).With().Str("version", version.Version).Timestamp().Logger()
@@ -16,4 +30,13 @@ func main() {
 
 	result := deleteme.Add(1, 2)
 	logger.Info().Msgf("result %d", result)
+
+	db := getcreds("CTS")
+	logger.Info().Msgf("getting %s", db)
+
+	conncheck := checktcp(5432)
+	logger.Info().Msgf(conncheck)
+
+	auth := dblogin(db)
+	logger.Info().Msg(auth)
 }
---
+++ deleteme/deleteme_test.go
@@ -0,0 +1,28 @@
+package deleteme
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "adding shit",
+			args: args{
+				a: 15,
+				b: 10,
+			}, want: 25},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Add(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
